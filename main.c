1929e6a291c0edb9992926de447a569ed1044b8f
Tharun Kothari <tharunkothari@yahoo.com>
2023-08-02 13:31:19 +0530

    Right Collision added

--- main.c
+++ main.c
@@ -3,26 +3,27 @@
 
 #define FPS 60 
 #define dt 0.01
+#define screenWidth 960
+#define screenHeight 600
+
 
 Vector2 nextPosition(Vector2 currentPosition, Vector2 Velocity);
-void collide(Vector2 ballPosition, Vector2 Velocity);
+void collide(Vector2 ballPosition, Vector2 *Velocity);
 
 int main(void) {
 	   
-	const int screenWidth = 960;
-	const int screenHeight = 600;
-
 	InitWindow(screenWidth, screenHeight, "Marching Squares");
 
 	SetTargetFPS(FPS);
 
 	Vector2 ballPosition = { (float) screenWidth/2, (float) screenHeight/2 };
-	Vector2 Velocity = { 100, 100 };
+	Vector2 Velocity = { 100, 0 };
 	while(!WindowShouldClose()) {
 		BeginDrawing();
 
 		ClearBackground(BLACK);
 		DrawCircleV(ballPosition, 50, PINK);
+		collide(ballPosition, &Velocity);
 		ballPosition = nextPosition(ballPosition, Velocity);
 		EndDrawing();
 	}
@@ -39,8 +40,10 @@ Vector2 nextPosition(Vector2 currentPosition, Vector2 Velocity) {
 }
 
 
-void collide(Vector2 ballPosition, Vector2 Velocity) {
-	
+void collide(Vector2 ballPosition, Vector2 *Velocity) {
+	if (ballPosition.x > screenWidth) {
+		Velocity->x = -Velocity->x;
+	}
 }
 
 
